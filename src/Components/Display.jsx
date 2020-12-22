3b6e635f11ddfc82dc6ae626ead32c3a3056d5a1
Edward Blanciak <blanciak.edward2@gmail.com>
2020-12-22 13:36:19 -0500

    display, form started

---
+++ src/Components/Display.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+		maxWidth: 752,
+	},
+	demo: {
+		backgroundColor: theme.palette.background.paper,
+	},
+	title: {
+		margin: theme.spacing(4, 0, 2),
+	},
+}));
+
+function generate(element) {
+	return [0, 1, 2, 3].map((value) =>
+		React.cloneElement(element, {
+			key: value,
+		})
+	);
+}
+
+const Display = () => {
+	const classes = useStyles();
+	const [secondary, setSecondary] = React.useState(true);
+
+	return (
+		<div className={classes.root}>
+			<Grid container spacing={2}>
+				<Grid item xs={12} md={6}>
+					<Typography
+						variant='h6'
+						className={classes.title}
+					>
+						Results
+					</Typography>
+					<div className={classes.demo}>
+						<List>
+							{generate(
+								<ListItem>
+									<ListItemText
+										primary='Single-line item'
+										secondary={
+											secondary
+												? 'Secondary text'
+												: null
+										}
+									/>
+								</ListItem>
+							)}
+						</List>
+					</div>
+				</Grid>
+			</Grid>
+		</div>
+	);
+};
+
+export default Display;
---
+++ src/Components/Form.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import { TextField, Button } from '@material-ui/core';
+
+const FormContainer = styled.div`
+	display: flex;
+	justify-content: center;
+`;
+
+const FormWrapper = styled.form`
+	display: flex;
+	flex-direction: column;
+	width: 50%;
+`;
+
+const Form = () => {
+	return (
+		<FormContainer>
+			<FormWrapper>
+				<TextField multiline label='Number Set' />
+				<Button type='submit'>Submit</Button>
+			</FormWrapper>
+		</FormContainer>
+	);
+};
+
+export default Form;
